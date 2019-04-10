f27752d9b7c106f61e5c9c5f19e9d36c34352f10
ds <dsada@gmail.com>
2019-04-10 12:08:08 -0300

    Clase 8

--- Clases/Clase 8 Estructuras 10 de abril/Estructuras/main.c
+++ Clases/Clase 8 Estructuras 10 de abril/Estructuras/main.c
@@ -14,38 +14,36 @@ struct datosPersonales
 };
 int main()
 {
-    struct datosPersonales vecAgenda[3];
+    struct datosPersonales agenda;
 
-        for(int i=0;i<3;i++)
-            {
                 printf("Ingrese el nombre:\n");
-                gets(vecAgenda[i].nombre);
+                gets(agenda.nombre);
                 fflush(stdin);
 
                 printf("Ingrese el apellido:\n");
-                gets(vecAgenda[i].apellido);
+                gets(agenda.apellido);
                 fflush(stdin);
 
                 printf("Ingrese la localidad:\n");
-                gets(vecAgenda[i].localidad);
+                gets(agenda.localidad);
                 fflush(stdin);
 
                 printf("Ingrese el nombre de la calle:\n");
-                gets(vecAgenda[i].calle);
+                gets(agenda.calle);
                 fflush(stdin);
 
                 printf("Ingrese codigo postal:\n");
-                scanf("%d", &vecAgenda[i].codigoPostal);
+                scanf("%d", &agenda.codigoPostal);
 
                 printf("Ingrese el numero de la calle:\n");
-                scanf("%d", &vecAgenda[i].numero);
+                scanf("%d", &agenda.numero);
 
-            }
 
-                printf("El nombre es %s", vecAgenda[i].nombre);
-                printf("\nEl apellido es %s", vecAgenda[i].apellido);
-                printf("\nVive en la localidad %s de codigo postal %d", vecAgenda[i].localidad, vecAgenda[i].codigoPostal);
-                printf("\nReside en la calle %s %d", vecagenda[i].calle, vecAgenda[i].numero);
+
+                printf("El nombre es %s", agenda.nombre);
+                printf("\nEl apellido es %s", agenda.apellido);
+                printf("\nVive en la localidad %s de codigo postal %d", agenda.localidad, agenda.codigoPostal);
+                printf("\nReside en la calle %s %d", agenda.calle, agenda.numero);
         return 0;
 
 
